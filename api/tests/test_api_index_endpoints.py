485fec948d3105248fa7e3e2e2fbd0da09513233
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2022-08-12 13:27:42 -0400

    feat: Added custom exception handler to python api.

--- api/tests/test_api_index_endpoints.py
+++ api/tests/test_api_index_endpoints.py
@@ -3,5 +3,23 @@ from ..index import app
 
 client = TestClient(app)
 
-def test_always_passes():
-	assert True
+def test_valid_id_path_param_1():
+	response = client.get('/nyccounty/1')
+	assert response.status_code == 200
+	assert response.json() == {
+		"data": "Bronx"
+	}
+
+def test_valid_id_path_param_5():
+	response = client.get('/nyccounty/5')
+	assert response.status_code == 200
+	assert response.json() == {
+		"data": "Richmond"
+	}
+
+def test_invalid_id_path_param():
+	response = client.get('/nyccounty/1000')
+	assert response.status_code == 400
+	assert response.json() == {
+		"error": "error"
+	}
