aea60ecc97c5a7d0e427dd1d99c03132ad788faa
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2022-10-16 11:23:32 -0400

    feat: Added updated components.

--- src/components/RickAndMortyCharacter/RickAndMortyCharacter.tsx
+++ src/components/RickAndMortyCharacter/RickAndMortyCharacter.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useApolloClient } from '@apollo/client';
 import { Character } from '../../apollo/generated/react-apollo';
+import StarRating from '../StarRating';
+import { GetRickAndMortyCharacterStarRatingDocument} from '../../apollo/generated/react-apollo';
 
 import { Thumbnail, ThumbnailImage } from '../../styles';
 
 function RickAndMortyCharacter({character, index}: {character: Character; index: number}) {
+	const [theStarRating, setTheStarRating] = useState<number | undefined>();
+
+	const client = useApolloClient();
+
+	useEffect(() => {
+		const { character: { rating } } = client.readQuery({
+			query: GetRickAndMortyCharacterStarRatingDocument,
+			variables: { id: character.id },
+		});
+		setTheStarRating(rating);
+	}, []);
+
 	return (
-		<div className="cursor-pointer">
-			<Thumbnail>
-				{character.image ? (
-					//@ts-ignore
-					<ThumbnailImage src={character.image} alt={character.name} />
-				) : (
-					<div className="text-center">
-						<i>Image not found</i>
-					</div>
-				)}
-			</Thumbnail>
-			<div className="text-center">
-				{character.name ? <h4>{character.name} <span><h5>{index+1}</h5></span></h4> : <i>Name not found</i>}
+		<>
+			<div className="mb-2">
+				<StarRating rating={theStarRating} />
 			</div>
-		</div>
+			<div className="cursor-pointer">
+				<Thumbnail>
+					{character.image ? (
+						//@ts-ignore
+						<ThumbnailImage src={character.image} alt={character.name} />
+					) : (
+						<div className="text-center">
+							<i>Image not found</i>
+						</div>
+					)}
+				</Thumbnail>
+				<div className="text-center">
+					{character.name ? <h4>{character.name} <span><h5>{index+1}</h5></span></h4> : <i>Name not found</i>}
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/pages/graphqlquery.tsx
+++ src/pages/graphqlquery.tsx
@@ -10,8 +10,7 @@ import Button from '../components/Button';
 import RickAndMortyCharacter from '../components/RickAndMortyCharacter/RickAndMortyCharacter';
 import Modal from '../components/Modal/Modal';
 import RickAndMortyModalView from '../components/RickAndMortyModalView/RickAndMortyModalView';
-import { GetAllRickAndMortyCharactersDocument } from '../apollo/generated/react-apollo';
-import StarRating from '../components/StarRating';
+import { GetAllRickAndMortyCharactersDocument} from '../apollo/generated/react-apollo';
 
 interface RickAndMortyPageProps {
 	documentTitle?: string;
@@ -27,7 +26,7 @@ const RickAndMorty: NextPage<RickAndMortyPageProps> = ({ documentTitle }) => {
 	const [queryError, setQueryError] = useState(false);
 	const [openModal, setOpenModal] = useState(false);
 	const [currentModalCharacter, setCurrentModalCharacter] = useState<any|null>(null);
-
+	
 	const {
 		loading,
 		error,
@@ -104,27 +103,6 @@ const RickAndMorty: NextPage<RickAndMortyPageProps> = ({ documentTitle }) => {
 
 	const client = useApolloClient();
 
-	//function onRenderCallback(
-	//	id, // the "id" prop of the Profiler tree that has just committed
-	//	phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
-	//	actualDuration, // time spent rendering the committed update
-	//	baseDuration, // estimated time to render the entire subtree without memoization
-	//	startTime, // when React began rendering this update
-	//	commitTime, // when React committed this update
-	//	interactions // the Set of interactions belonging to this update
-	//) {
-	//	// Aggregate or log render timings...
-	//}
-
-	//const logTimes = (id, phase, actualTime, baseTime, startTime, commitTime) => {
-	//	console.log(`${id}'s ${phase} phase:`);
-	//	console.log(`Actual time: ${actualTime}`);
-	//	console.log(`Base time: ${baseTime}`);
-	//	console.log(`Start time: ${startTime}`);
-	//	console.log(`Commit time: ${commitTime}`);
-	//	console.log(`==============================================`);
-	//};
-
 	return (
 		<>
 			<Head>
@@ -178,9 +156,6 @@ const RickAndMorty: NextPage<RickAndMortyPageProps> = ({ documentTitle }) => {
 					<div className="row-grid-rickandmorty">
 						{data && data.characters.results.map((character: Character, index:number) => (
 							<div key={index} className="mb-3 container-padding-border-radius-2">
-								<div className="mb-2">
-									<StarRating rating={ (Math.floor(Math.random() * (100 - 1)) + 1) } />
-								</div>
 								<div
 									onClick={() => {
 										toggleModal();
