c01ca2ab9ade7050187f66bfa8fc3eb0c573bd00
AlexSmith716 <1691290+alexsmith716@users.noreply.github.com>
2022-11-12 16:39:38 -0500

    feat: Implement cypress webpack preprocessor.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'cypress';
+import webpackPreprocessor from "@cypress/webpack-preprocessor";
 
 export default defineConfig({
 	watchForFileChanges: false,
@@ -7,7 +8,15 @@ export default defineConfig({
 
 	e2e: {
 		baseUrl: 'http://localhost:3000',
-		specPattern: 'cypress/e2e/*_spec.{ts,tsx}',
+		specPattern: 'cypress/e2e/*_spec.{js,jsx,ts,tsx}',
 		supportFile: false,
+		setupNodeEvents(on,) {
+			// ************ probably can modify & use Cypress's default webpack config here ***********
+			// const options = webpackPreprocessor.defaultOptions;
+			// options.webpackOptions.module.rules[0].use[0].options.presets.push('');
+			// options.webpackOptions.module.rules[0].use[0].options.plugins = ['',];
+			const options = { webpackOptions: require('./webpack.config.js') };
+			on('file:preprocessor', webpackPreprocessor(options));
+		}
 	},
 });
